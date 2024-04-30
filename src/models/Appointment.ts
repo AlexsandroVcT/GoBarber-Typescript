203b47b1ef1507ac165da2886630adbd1e3609fa
AlexsandroVct <alexsandro.vct@outlook.com>
2024-04-30 20:54:42 -0300

    Primeiro commit

---
+++ src/models/Appointment.ts
@@ -0,0 +1,24 @@
+// entidade de agendamento
+
+import { uuid } from 'uuidv4'
+
+interface AppointmentConstructor {
+	provider: string
+	date: Date
+}
+
+class Appointment {
+	id: string
+
+	provider: string
+
+	date: Date
+
+	constructor({ provider, date }: Omit<Appointment, 'id'>) {
+		this.id = uuid()
+		this.provider = provider
+		this.date = date
+	}
+}
+
+export default Appointment
